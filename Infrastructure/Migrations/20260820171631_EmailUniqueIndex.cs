fff924e66f0ff91f8222ed9dea4fe0ff36f2619e
SkizerskiyX <exalizer@gmail.com>
2026-08-20 10:17:55 -0700

    Add case-insensitive email unique index migration with IF NOT EXISTS

---
+++ Infrastructure/Migrations/20260820171631_EmailUniqueIndex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "CREATE UNIQUE INDEX IF NOT EXISTS ux_users_email_lower ON \"Users\" (lower(\"Email\"));");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX IF EXISTS ux_users_email_lower;");
+        }
+    }
+}
